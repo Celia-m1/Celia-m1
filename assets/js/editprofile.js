b50c68c646272dce2619f78c30d4d3b6cd341873
Yasmine <yasmine.meriche@ensia.edu.dz>
2024-11-14 15:59:30 +0100

    java script for forms added

--- assets/js/editprofile.js
+++ assets/js/editprofile.js
@@ -1,4 +1,5 @@
 function validation(){
+    var valid=true;
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var mobilephone = document.getElementById('mobilephone').value;
@@ -6,14 +7,86 @@ function validation(){
     
     if (username==""){
         document.getElementById('usernamee').innerHTML="**Please write your username in the box**";
+        valid=false;
+    }else{
+        document.getElementById('usernamee').innerHTML = "";
     }
+    
     if (email==""){
         document.getElementById('emaill').innerHTML="**Please write your email in the box**";
+        valid=false;
+    }else{
+        document.getElementById('emaill').innerHTML = "";
     }
     if (mobilephone==""){
         document.getElementById('mobilephonee').innerHTML="**Please write your phone number in the box**";
+        valid=false;
+    }else{
+        document.getElementById('mobilephonee').innerHTML = "";
     }
+    if(mobilephone.length!=10){
+        document.getElementById('mobilephonee').innerHTML="**The number must contain exactly 10 digits** "
+        valid=false;
+    }else{if(mobilephone.length===10 && mobilephone[0]!='0'){
+        document.getElementById('mobilephonee').innerHTML="**The phone number must start with 0**"
+        valid=false;
+    }else {
+        document.getElementById('mobilephonee').innerHTML="";
+
+    }}
     if (location==""){
         document.getElementById('locationn').innerHTML="**Please write your location in the box**";
+        valid=false;
+    }else{
+        document.getElementById('locationn').innerHTML = "";
     }
+    if (username.length<6){
+        document.getElementById('usernamee').innerHTML="**the username must contain at least 6 characters**"
+        valid=false;
+    }
+   
+    
+    return valid;
+
+}
+function validationemail(){
+    var valid=false;
+    var email = document.getElementById('email').value;
+    var emailPattern = /^[A-Za-z._\-0-9]+@[A-Za-z]+\.[a-z]{2,4}$/;
+    if(!email.match(emailPattern)){
+        document.getElementById('emaill').innerHTML="**Please enter a valid email**";
+        
+    }else{
+        document.getElementById('emaill').innerHTML = "";
+        valid=true;
+    }
+    return valid;
+
+
+}
+function handleSubmit() {
+    if (validation()&&validationemail()){
+    
+    playAudiosuccess();
+    setTimeout(transformButtonToDone, 1000);
+
+}else{
+    playAudiofail();
+
+}
+}
+function playAudiofail(){
+    const audio= new Audio();
+    audio.src ="../images/fail.wav";
+    audio.play();
+}
+function playAudiosucces(){
+    const audio= new Audio();
+    audio.src ="../images/click.wav";
+    audio.play();
+}
+function transformButtonToDone(){
+    const submitButton =document.getElementById('submitButton');
+    submitButton.innerHTML="✅ Done"
+    submitButton.disabled = true;
 }
--- assets/js/formbuy.js
+++ assets/js/formbuy.js
@@ -1,4 +1,5 @@
 function validation (){
+    var valid=true;
     var title = document.getElementById('title').value ;
     var author = document.getElementById('author').value ;
     var condition= document.getElementById('condition').value ;
@@ -11,29 +12,95 @@ function validation (){
 
     if (title==""){
         document.getElementById('titlee').innerHTML="**Please write title in the box**";
+        valid=false;
+    }else{
+        document.getElementById('titlee').innerHTML="";
+        
     }
     if (author==""){
         document.getElementById('authorr').innerHTML="**Please write the name of the author in the box**";
+        valid=false;
+    }else{
+        document.getElementById('authorr').innerHTML="";
+        
     }
     if (condition==""){
         document.getElementById('conditionn').innerHTML="**Please write condition in the box**";
+        valid=false;
+    }else{
+        document.getElementById('conditionn').innerHTML="";
     }
     if (edition==""){
         document.getElementById('editionn').innerHTML="**Please write title in the box**";
+        valid=false;
+    }else{
+        document.getElementById('editionn').innerHTML="";
     }
     if (year==""){
         document.getElementById('yearr').innerHTML="**Please write the year of publication in the box**";
+        valid=false;
+    }else{
+        document.getElementById('yearr').innerHTML="";
+    }
+    if (year.length!=4){
+        document.getElementById('yearr').innerHTML="**Please enter a correct year**";
     }
     if (language==""){
         document.getElementById('languagee').innerHTML="**Please write the language in the box**";
+        valid=false;
+    }else{
+        document.getElementById('languagee').innerHTML="";
     }
     if (pages==""){
         document.getElementById('pagess').innerHTML="**Please write number of pages in the box**";
+        valid=false;
+    }else{
+        document.getElementById('pagess').innerHTML="";
     }
+
     if (summary==""){
         document.getElementById('summaryy').innerHTML="**Please write the summary  in the box**";
+        valid=false;
+    }else{
+        document.getElementById('summaryy').innerHTML="";
     }
     if (price==""){
         document.getElementById('pricee').innerHTML="**Please write the price in the box**";
+        valid=false;
+    }else{
+        document.getElementById('pricee').innerHTML="";
     }
+    if (price.length>4){
+        document.getElementById('pricee').innerHTML="**The price is not reasonable**";
+        valid=false;
+    
+    }
+    return valid;
+}
+function handleSubmit() {
+    if (validation()){
+    
+    playAudiosucces();
+    setTimeout(transformButtonToDone, 1000);
+
+}else{
+    playAudiofail();
+
+}
+}
+
+function playAudiofail(){
+    const audio= new Audio();
+    audio.src ="../images/fail.wav";
+    audio.play();
+}
+function playAudiosucces(){
+    const audio= new Audio();
+    audio.src ="../images/click.wav";
+    audio.play();
+}
+function transformButtonToDone(){
+    const submitButton =document.getElementById('submitButton');
+    submitButton.innerHTML="✅ Done"
+    submitButton.disabled = true;
 }
